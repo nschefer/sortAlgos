40fa19974c0b7e3a4815220f5d18afed0056441d
Noah Schefer <nbschefer@gmail.com>
2019-05-23 12:15:18 -0400

    Added pointer solution for merge

--- mergesort.js
+++ mergesort.js
@@ -6,28 +6,52 @@ function split(wholeArray) {
   return [firstHalf, secondHalf];
 }
 
-function merge(arr1, arr2) {
-  const mergedArr = [];
-  const maxLength = arr1.length + arr2.length;
+//noah and sri original solution
+// function merge(arr1, arr2) {
+//   const mergedArr = [];
+//   const maxLength = arr1.length + arr2.length;
 
-  while (mergedArr.length !== maxLength) {
-    if (arr1[0] < arr2[0]) {
-      mergedArr.push(arr1.shift());
-    } else if (arr2[0] <= arr1[0]) {
-      mergedArr.push(arr2.shift());
+//   while (mergedArr.length !== maxLength) {
+//     if (arr1[0] < arr2[0]) {
+//       mergedArr.push(arr1.shift());
+//     } else if (arr2[0] <= arr1[0]) {
+//       mergedArr.push(arr2.shift());
+//     } else {
+//       mergedArr.push(arr1[0] ? arr1.shift() : arr2.shift());
+//     }
+//   }
+
+//   return mergedArr;
+// }
+
+//solution w/pointers and not mutating array
+function merge(leftArr, rightArr) {
+  const mergedArr = [];
+  let left = 0;
+  let right = 0;
+
+  while (left < leftArr.length && right < rightArr.length) {
+    if (leftArr[left] < rightArr[right]) {
+      mergedArr.push(leftArr[left]);
+      left++;
     } else {
-      mergedArr.push(arr1[0] ? arr1.shift() : arr2.shift());
+      mergedArr.push(rightArr[right]);
+      right++;
     }
   }
 
-  return mergedArr;
+  return mergedArr.concat(leftArr.slice(left)).concat(rightArr.slice(right));
 }
 
 function mergeSort(array) {
   if (array.length === 1 || !array.length) return array;
   const [firstHalf, secondHalf] = split(array);
-  const mergedFirst = mergeSort(firstHalf);
-  const mergedSecond = mergeSort(secondHalf);
-  const mergedArr = merge(mergedFirst, mergedSecond);
-  return mergedArr;
+
+  //noah and sri original solution
+  // const mergedFirst = mergeSort(firstHalf);
+  // const mergedSecond = mergeSort(secondHalf);
+  // const mergedArr = merge(mergedFirst, mergedSecond);
+  // return mergedArr;
+
+  return merge(mergeSort(firstHalf), mergeSort(secondHalf));
 }
